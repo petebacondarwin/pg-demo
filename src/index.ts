e9aa2398d8c99233a446b86cea11948d8ce35373
Pete Bacon Darwin <pete@bacondarwin.com>
2023-05-05 10:26:01 +0100

    Simple demo of node-postgres

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import { Client } from "pg";
+import html from "./index.html";
+
+export interface Env {
+  DB: string;
+}
+
+export default {
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const url = new URL(request.url);
+    if (url.pathname === "/favicon.ico")
+      return new Response(null, { status: 404 });
+
+    if (url.pathname === "/") {
+      return new Response(html, {
+        headers: { "Content-Type": "text/html; charset=utf-8" },
+      });
+    }
+
+    const params = url.searchParams;
+
+    var client = new Client(env.DB);
+    await client.connect();
+    const result = await client.query({
+      text: "SELECT * from cars WHERE YEAR < $1",
+      values: [params.get("filter")],
+    });
+    console.log(JSON.stringify(result.rows));
+    const resp = Response.json(result.rows);
+    // Clean up the client, ensuring we don't kill the worker before that is completed.
+    ctx.waitUntil(client.end());
+    return resp;
+  },
+};
---
+++ src/types.d.ts
@@ -0,0 +1,4 @@
+module "*.html" {
+  const content: string;
+  export default content;
+}
